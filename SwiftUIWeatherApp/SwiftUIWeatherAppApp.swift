512dc5b61b367ee8f27cc22fb14396ae23f1c2e9
Christian Teguim <cteguimtu620@gmail.com>
2024-09-20 15:26:50 -0400

    Initial Commit

---
+++ SwiftUIWeatherApp/SwiftUIWeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIWeatherAppApp.swift
+//  SwiftUIWeatherApp
+//
+//  Created by Christian Teguim on 2024-09-20.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIWeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SwiftUIWeatherAppTests/SwiftUIWeatherAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIWeatherAppTests.swift
+//  SwiftUIWeatherAppTests
+//
+//  Created by Christian Teguim on 2024-09-20.
+//
+
+import Testing
+@testable import SwiftUIWeatherApp
+
+struct SwiftUIWeatherAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
